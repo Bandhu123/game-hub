1f41060b696c4feef677451484f957b64c57d034
Bandhu123 <ravi.iitk06@gmail.com>
2025-08-31 21:46:41 +0530

    Handle no image games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,7 @@
-
+import noImage from '../assets/no-image-placeholder.webp';
 
 const croppedImageURL = (url:string) => {
-  if(!url) return '';
+  if(!url) return noImage;
 const endpoint='media/';
   const urlSplitted = url.split(endpoint);
   const croppedUrl = urlSplitted[0]+"media/" + "crop/600/400/"+urlSplitted[1];
